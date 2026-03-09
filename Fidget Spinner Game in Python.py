1c2a66f835eeed10881b95aef4e38ebc0c58be97
Rohanskadu7309 <rohanskadu7309@gmai.com>
2026-03-09 22:04:35 +0530

    added on click run

--- Fidget Spinner Game in Python.py
+++ Fidget Spinner Game in Python.py
@@ -1,8 +1,10 @@
 from turtle import *
+
 state = {'turn': 0}
+
 def spinner():
     clear()
-    angle = state['turn']/10
+    angle = state['turn'] / 10
     right(angle)
     forward(100)
     dot(120, 'red')
@@ -17,20 +19,27 @@ def spinner():
     back(100)
     right(120)
     update()
+
 def animate():
-    if state['turn']>0:
-        state['turn']-=1
+    if state['turn'] > 0:
+        state['turn'] -= 1
 
     spinner()
     ontimer(animate, 20)
-def flick():
-    state['turn']+=10
+
+# The mouse click passes 'x' and 'y' coordinates, 
+# so we add them as arguments even if we don't use them.
+def flick(x, y):
+    state['turn'] += 10
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 width(20)
-onkey(flick, 'space')
+
+# Changed from onkey to onscreenclick
+onscreenclick(flick) 
+
 listen()
 animate()
 done()
